922e5822f6647c593b98eb59e4074c0ce8ddf1e7
conno224-glitch <conno224@gmail.com>
2026-04-30 20:58:04 -0300

    Create aula4-exercicio1.c
    exercicio focado em calculo

---
+++ aula4-exercicio1.c
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int main(void) {
+	float numero1;
+	float numero2;
+	char operacao;
+	
+	printf("Digite o primeiro numero: ");
+	scanf("%f", &numero1);
+	printf("Digite a operacao (+, -, *, /): ");
+	scanf(" %c", &operacao);
+	printf("Digite o segundo numero: ");
+	scanf("%f", &numero2);
+	
+	switch (operacao) {
+		case '/':
+			if (numero2 != 0) {
+			    printf("Resultado: %.2f\n", numero1 / numero2);
+			}else {
+				printf("Nao e possivel dividir por zero.\n");
+			}
+			break;
+		case '+':
+			printf("Resultado: %f\n", numero1 + numero2);
+			break;
+		case '-':
+			printf("Resultado: %f\n", numero1 - numero2);
+			break;
+		case '*':
+			printf("Resultado: %f\n", numero1 * numero2);
+			break;   
+	}  
+return 0;
+}
