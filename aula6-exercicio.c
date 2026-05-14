4b67df028def745435f0f92d53848053843bcc11
conno224-glitch <conno224@gmail.com>
2026-05-14 20:30:07 -0300

    Create aula6-exercicio.c
    exercicio

---
+++ aula6-exercicio.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void) {
+float notas[5];
+float soma = 0;
+float media;
+int acimadamedia = 0;
+
+for (int i = 0; i < 5; i++) {
+	printf("Digite a nota %d: " , i + 1);
+	scanf("%f", &notas[i]);
+	soma += notas[i];
+}
+media = soma / 5;
+for (int i = 0; i < 5; i++) {
+	if (notas[i] > media) {
+		acimadamedia++;
+	}
+}
+printf("\nMedia da turma: %.2f\n", media);
+printf("Quantidade de notas acima da media: %d\n", acimadamedia);
+
+
+
+
+
+	return 0;
+ }
